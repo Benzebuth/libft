e6e30452ec96154b2194cc49a841518b0861af22
Benzébuth <benjamin.colin.dev@gmail.com>
2021-10-16 00:34:52 +0200

    minor update

--- srcs/memory/ft_memset.c
+++ srcs/memory/ft_memset.c
@@ -15,12 +15,12 @@ void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t	count;
 
-	count = 0;
 	if (!b)
 		return (0);
+	count = 0;
 	while (count < len)
 	{
-		*(unsigned char *)(b + count) = (unsigned char)c;
+		*(unsigned char *)(b + count) = c;
 		count++;
 	}
 	return (b);
